78738270bb670ad21267efa0aec280555bb760d3
nathanielroy <74504039+nathanielroy@users.noreply.github.com>
2021-12-29 00:36:30 +0530

    Initial files

---
+++ src/main/java/com/tweetapp/tweets/TweetsApplication.java
@@ -0,0 +1,13 @@
+package com.tweetapp.tweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetsApplication.class, args);
+	}
+
+}
