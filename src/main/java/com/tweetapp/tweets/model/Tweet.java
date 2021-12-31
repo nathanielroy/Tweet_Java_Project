4e8163258f7a21a66fba53b1e1814e3aee3857e1
nathanielroy <nathanielroy97@gmail.com>
2022-01-01 01:16:05 +0530

    Basics CRUD operations

--- src/main/java/com/tweetapp/tweets/model/Tweet.java
+++ src/main/java/com/tweetapp/tweets/model/Tweet.java
@@ -26,6 +26,39 @@ public class Tweet {
 		this.email = email;
 	}
 
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	@Override
 	public String toString() {
 		return "Tweet [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
