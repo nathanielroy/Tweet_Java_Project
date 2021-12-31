eb78aaa2bead30e6b4d42a33fa6e7479b19d2a77
nathanielroy <nathanielroy97@gmail.com>
2021-12-31 21:14:46 +0530

    Database connection properties

--- src/main/java/com/tweetapp/tweets/TweetsApplication.java
+++ src/main/java/com/tweetapp/tweets/TweetsApplication.java
@@ -2,8 +2,10 @@ package com.tweetapp.tweets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class TweetsApplication {
 
 	public static void main(String[] args) {
