a78fcaf4f896a36b890de37d11307e9c8176f2d0
Nathaniel Roy <nathanielroy97@gmail.com>
2022-01-01 14:08:01 +0530

    added additional fields

--- src/main/java/com/tweetapp/tweets/model/Tweet.java
+++ src/main/java/com/tweetapp/tweets/model/Tweet.java
@@ -27,5 +27,78 @@ public class Tweet {
 
 	}
 
+	public Tweet(String firstName, String lastName, String email, String username, Gender gender, TweetsByUser tweets) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.gender = gender;
+		this.tweets = tweets;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public TweetsByUser getTweets() {
+		return tweets;
+	}
+
+	public void setTweets(TweetsByUser tweets) {
+		this.tweets = tweets;
+	}
+
+	@Override
+	public String toString() {
+		return "Tweet [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", gender=" + gender + ", tweets=" + tweets + "]";
+	}
+
+	
 	
 }
